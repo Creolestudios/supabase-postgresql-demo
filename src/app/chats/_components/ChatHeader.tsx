438848534b6a4299828a9a50ceb9ea3b4bd74764
ankitcreolestudios <ankit.kumar@creolestudios.com>
2024-03-05 07:04:26 +0530

    supbabase chat

--- src/app/chats/_components/ChatHeader.tsx
+++ src/app/chats/_components/ChatHeader.tsx
@@ -6,7 +6,7 @@ const ChatHeader = () => {
     <div className='h-20'>
       <div className='p-5 border-b border-b-green-200 h-full'>
         <div>
-          <h1 className='font-bold text-[20px] pb-[2px]'>Sup Next Chat</h1>
+          <h1 className='font-bold text-[20px] pb-[2px]'>Supabase Next Chat</h1>
           <OnlineUsers />
         </div>
       </div>
